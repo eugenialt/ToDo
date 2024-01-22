9fc8daded6522528511238fa3a734c1cae82ddcb
Eugenia Litvintsava <26181412mail.ru@gmail.com>
2024-01-22 17:09:25 +0300

    ДЗ по событиям/2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ root.append(header)
 header.append(container)
 container.append(headerInner)
 
-function createButton(numberButton, className) {
+function createButtons(numberButton, className) {
     const button = [];
     for (let i = 0; i < numberButton; i++){
         const buttons = document.createElement('button');
@@ -23,7 +23,7 @@ function createButton(numberButton, className) {
     return button;
 }
 
-const button = createButton(3, 'btn'); 
+const button = createButtons(3, 'btn'); 
 
 if (button.length >= 1) {
   button[0].textContent = 'Delete All';
@@ -36,8 +36,11 @@ if (button.length >= 3) {
 }
 
 button.forEach(button => {
-  headerInner.appendChild(button); // Добававляем каждую кнопку внутрь элемента
+  headerInner.appendChild(button); // Добавляем каждую кнопку внутрь элемента
 } );
+
+button[1].className = 'btn delete_button';
+headerInner.append(button[1])
 
 function insertLineInput(placeholderText,prevElement,nextElement ) {
   const lineInput = document.createElement('input');
@@ -65,7 +68,7 @@ headerUnder.append(allTask,completedTask)
 
 
 // Кнопки внутри шапки --2--
-const buttonUnderHeader = createButton(2, 'btn'); 
+const buttonUnderHeader = createButtons(2, 'btn'); 
 
 buttonUnderHeader.forEach(button => {
   headerUnder.appendChild(button);
@@ -86,27 +89,8 @@ const form = document.createElement('form');
 form.className = 'form';
 root.append(form)
 
-const tasks = [
-  {
-  id: self.crypto.randomUUID(),
-  task: 'Выучить JS',
-  isCompleted: false,
-  date: new Date().toLocaleDateString('ru-RU', { // либо так new Date().toLocaleDateString()
-    year: '2-digit',
-    month: 'short',
-    day: '2-digit'
-  })
- },
- {
-  id: self.crypto.randomUUID(),
-  task: 'Выучить JS',
-  isCompleted: false,
-  date: new Date().toLocaleDateString()
- },
-]
 
- 
-
+// Input
 const createInput = (type, placeholder, name) => {
   const input = document.createElement('input');
   input.type = type;
@@ -115,25 +99,130 @@ const createInput = (type, placeholder, name) => {
   return input;
 }
 
+// --3-- Блок с задачами и их свойствами
+const tasksContainer = document.createElement('div')
+
 const blockTask = document.createElement('div');
 blockTask.className = 'block_task';
-form.append(blockTask);
-const createTaskItem = (text,isCompleted, id, date) => {
+tasksContainer.append(blockTask);
+const createTaskItem = (text, isCompleted, id, date) => {
   const taskItem = document.createElement('div');
+  // Checkbox
   const checkbox = createInput('checkbox', '', 'checkbox');
   checkbox.checked = isCompleted;
-  checkbox.classList.add('checkbox');
+  checkbox.classList.add('check_input'); 
+  // Label for text in Checkbox
+  const label = document.createElement('label');
+  label.classList.add('label');
+  label.append(checkbox, text);
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      label.style.textDecoration = 'line-through';
+      taskItem.style.backgroundColor = '#101229';
+    } else {
+      label.style.textDecoration = 'none';
+      taskItem.style.backgroundColor = 'transparent';
+    }
+  });
+//  Span for graphic in Checkbox
+  const spanCheckbox = document.createElement('span');
+  spanCheckbox.classList.add('check_box');
+  label.append(spanCheckbox);
+  // Img (check)
+  const img = document.createElement('img');
+  img.classList.add('span_img');
+  img.src = 'img/icons8-галочка.svg'; 
+  spanCheckbox.appendChild(img);
+  img.style.display = 'none';
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      img.style.display = 'inline-block';
+    } else {
+      img.style.display = 'none';
+    }
+  });
+  // Date
   const currenData = document.createElement('span');
   currenData.className = 'curren_data';
   currenData.textContent = date;
+  // Button to delete task
+  const crossTask = document.createElement('button');
+  crossTask.className = 'btn cross_task';
+  crossTask.textContent = 'X';
+  // TaskItem elements
   taskItem.className = 'task_item';
   taskItem.id = id;
-  taskItem.textContent = text;
-  taskItem.append(checkbox, currenData);
+  taskItem.append(crossTask)
+  taskItem.append(label, currenData, crossTask);
   return taskItem;
 }
 
-tasks.forEach(task => {
-  const taskItem = createTaskItem(task.task, task.isCompleted, task.id, task.date);
-  blockTask.append(taskItem);
-})
+const task = {
+  task: "New task",
+  isCompleted: false,
+  date: new Date().toLocaleDateString(),
+  id: self.crypto.randomUUID(),
+};
+
+
+
+form.append(tasksContainer);
+const renderTasks = () => {
+  tasksContainer.innerHTML = '';
+  const tasks = getDataLocalStorage('tasks');
+  if (tasks && tasks.length > 0) {
+    tasks.forEach(({ id, text, date, isCompleted }) => {
+      const taskItem = createTaskItem(id, text, date, isCompleted);
+      tasksContainer.append(taskItem);
+    })
+  } else {
+    tasksContainer.innerText = 'No tasks';
+  }
+}
+
+
+// Add task -- добавляем задачи
+button[2].addEventListener('click', addTask);
+
+function addTask (){
+const taskItem = createTaskItem(task.task, task.isCompleted, task.id, task.date);
+blockTask.append(taskItem);
+}
+
+// Delete all task -- удаляем все задачи
+function deleteAllTask (){
+  if (confirm('Are you sure you want to delete all tasks?')) {
+    localStorage.removeItem('tasks'); // Удаляем все задачи из локального хранилища
+    blockTask.innerHTML = ''; 
+  } else {
+    alert('Be careful!');
+  }
+};
+
+button[0].addEventListener('click', deleteAllTask);
+
+
+// Delete task -- удаляем определённую задачу
+const deleteTask = (id) => {
+  const tasks = getDataLocalStorage('tasks');
+  if (tasks) {
+    const updatedTasks = tasks.filter((item) => item.id !== id);
+    setDataLocalStorage('tasks', updatedTasks);
+    renderTasks();
+  } else {
+    console.error('No task');
+  }
+}
+
+const handlerDeleteTask = (event) => {
+  const { target } = event;
+  
+  if (target.classList.contains('cross_task') || target.classList.contains('checkbox')) {
+    if (confirm('Delete task?')) {
+      const taskCard = target.closest('.task_item'); // Получаем родительскую карточку
+      const taskId = taskCard.id;
+      deleteTask(taskId); 
+      taskCard.remove(); // Удаляем карточку из DOM
+    }
+  }
+}
